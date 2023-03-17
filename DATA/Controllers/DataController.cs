97ff99b505564b744fa1aa56af5232860eb28c07
Karthick Ganesan <karthick.ganesh@outlook.com>
2023-03-17 17:21:06 +0530

    Add project files.

---
+++ DATA/Controllers/DataController.cs
@@ -0,0 +1,98 @@
+﻿using DATA.DAL;
+using DATA.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DATA.Controllers
+{
+    public class DataController : Controller
+    {
+        private readonly DataDAL _dal;
+        public DataController(DataDAL dal)
+        {
+            _dal = dal; 
+        }
+        // GET: DataController
+        public IActionResult Index()
+        {
+            List<Data> data = new List<Data>();
+            data = _dal.GetAll();
+            return View(data);
+        }
+
+        // GET: DataController/Details/5
+        public ActionResult Details(int id)
+        {
+ 
+            return View();
+        }
+
+        // GET: DataController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DataController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Data data)
+        {
+            try
+            {
+                bool result = _dal.Insert(data);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DataController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            Data data = _dal.GetById(id);
+            return View(data);
+        }
+
+        // POST: DataController/Edit/5
+        [HttpPost]
+        public IActionResult Edit(Data data)
+        {
+            try
+            {
+                bool result = _dal.Update(data);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DataController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            Data data = _dal.GetById(id);
+            return View(data);
+
+        }
+
+        // POST: DataController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Data data)
+        {
+            try
+            {
+                bool result = _dal.Delete(data);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DATA/DAL/DataDAL.cs
@@ -0,0 +1,104 @@
+﻿using DATA.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DATA.DAL
+{
+    public class DataDAL
+    {
+        SqlConnection _connection = null;
+        SqlCommand _command = null;
+        public static IConfiguration Configuration { get; set; }
+        private string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            return Configuration.GetConnectionString("DefaultConnection");
+        }
+        public List<Data> GetAll()
+        {
+            List<Data> datalist = new List<Data>();
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "DATAVIEW_SP";
+                _connection.Open();
+                SqlDataReader dr = _command.ExecuteReader();
+                while(dr.Read())
+                {
+                    Data data = new Data();
+                    data.Id = Convert.ToInt32(dr["Id"]);
+                    data.Name = dr["Name"].ToString();
+                    data.Age = Convert.ToInt32(dr["Age"]);
+                    datalist.Add(data);
+                }
+            }
+            return datalist;
+        }
+        public bool Insert(Data data)
+        {
+            using(_connection=new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "DATAINSERT_SP";
+                _command.Parameters.AddWithValue("@Name", data.Name);
+                _command.Parameters.AddWithValue("@Age", data.Age);
+                _connection.Open();
+                _command.ExecuteNonQuery();
+            }
+            return true;
+        }
+        public Data GetById(int id)
+        {
+            Data data = new Data();
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.Parameters.AddWithValue("@id", id);
+                _command.CommandText = "DATAVIEWBYID_SP";
+                _connection.Open();
+                SqlDataReader dr = _command.ExecuteReader();
+                while (dr.Read())
+                {
+                    data.Id = Convert.ToInt32(dr["Id"]);
+                    data.Name = dr["Name"].ToString();
+                    data.Age = Convert.ToInt32(dr["Age"]);
+                }
+            }
+            return data;
+        }
+        public bool Update(Data data)
+        {
+            int id = 0;
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "DATAUPDATE_SP";
+                _command.Parameters.AddWithValue("@id", data.Id);
+                _command.Parameters.AddWithValue("@Name", data.Name);
+                _command.Parameters.AddWithValue("@Age", data.Age);
+                _connection.Open();
+                id = _command.ExecuteNonQuery();
+            }
+            return true;
+        }
+        public bool Delete(Data data)
+        {
+            int id = 0;
+            using (_connection = new SqlConnection(GetConnectionString()))
+            {
+                _command = _connection.CreateCommand();
+                _command.CommandType = CommandType.StoredProcedure;
+                _command.CommandText = "DATAUPDATE1_SP";
+                _command.Parameters.AddWithValue("@id", data.Id);
+                _connection.Open();
+                id = _command.ExecuteNonQuery();
+            }
+            return true;
+        }
+    }
+}
